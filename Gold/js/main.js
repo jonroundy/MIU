1f25544ae0e500177edf905f337d1baaccf430c3
Jon Roundy <jonroundy@fullsail.edu>
2012-08-17 01:36:23 -0500

    To tired going in circles will finish once home from work.

--- Gold/js/main.js
+++ Gold/js/main.js
@@ -1,47 +1,55 @@
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
-	
-}
+});
 		
-$('#addTask').on('pageinit', function(){
+$('#addTask').on('pageinit', function(data){
 
-		var tlform = $('#taskInfoForm');
-			//tlerrorlink = $('#taskFormErrors');
-		    tlform.validate({
+$(document).ready(function(){
+
+		var tlform = $('#taskInfoForm'),
+			tlerrorlink = $('#tl errorslink')
+		;
+		   // console.log(validator.submitted);
+		   
+		tlform.validate({
 			invalidHandler: function(form, validator) {
-			},
+				tlerrorlink.click();
+				for(var key in validator.submitted) {
+					var label = $('label[for^="'+ key +'"]');
+					label.closest('fieldset')
+		};
+	},
 			submitHandler: function() {
-		var data = tlform.serializeArray();
-			parseTaskData(data);
-		}
+				var data = tlform.serializeArray();
+				parseTaskData(data);
+		},
+	},
 	});
-	
 	//any other code needed for addItem page goes here
-	
 });
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
-var autofillData = function (){
-	 
-};
+//var autofillData = function (){
 
-var getData = function(){
+//});
 
-};
+//var getData = function(){
+
+//};
 
 var parseTaskData = function(data){
 	
-	console.log(data)
+	console.log(data);
 	
 }; 
 
-var	deleteItem = function (){
+//var	deleteItem = function (){
 			
-};
+//};
 					
-var clearLocal = function(){
+//var clearLocal = function(){
 
-};
+//};
 
 
